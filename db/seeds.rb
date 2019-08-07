d6e7630ff090bc177995aed8e07a3f2c7c48b00d
Alex Johnston <alexjohnston@ymail.com>
2019-08-07 10:57:54 +0100

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# Users
+User.create!(name:  "Example User",
+  username: "Username",
+  password:              "foobar",
+  avatar: "img_url")
+
+99.times do
+name  = Faker::Sports::Football.player
+username = Faker::Sports::Football.team
+password = "password"
+avatar = "img_url"
+User.create!(name:  name,
+    username: username,
+    password: password,
+    avatar: avatar)
+end
+
+# Matches
+
+Match.create!(user_id: 1,
+  sport: "Sport",
+  opponent_id: 2,
+  opponent_name: '',
+  user_score: 1,
+  opponent_score: 0)
+
+99.times do
+  user_id = rand(1..99),
+  sport = Faker::Game.title,
+  opponent_id = rand(1..99),
+  opponent_name = Faker::Sports::Football.player,
+  user_score = rand(1..20)
+  opponent_score = rand(1..20)
+end
+
+# Following relationships
+
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
