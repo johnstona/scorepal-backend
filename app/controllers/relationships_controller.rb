9401351e880737dc07fca2e673781db3a66d1f58
Alex Johnston <alexjohnston@ymail.com>
2019-08-07 14:40:56 +0100

    relationships backend functionality

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,20 @@
 class RelationshipsController < ApplicationController
 
+  def index
+    relationships = Relationship.all
+    render json: relationships, except: [:updated_at, :created_at]
+  end
+
   def create
-    user = User.find(params[:followed_id])
-    current_user.follow(user)
+    user1 = User.find(params[:follower_id])
+    user2 = User.find(params[:followed_id])
+    user1.follow(user2)
   end
 
   def destroy
-    user = Relationship.find(params[:id]).followed
-    current_user.unfollow(user)
+    user1 = User.find(params[:id])
+    user2 = Relationship.find(params[:id]).followed
+    user1.unfollow(user2)
   end
 
 end
