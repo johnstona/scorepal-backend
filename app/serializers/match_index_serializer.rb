90ef907edb8f2fbc09fb0e4d54e655128a5e6bcf
Alex Johnston <alexjohnston@ymail.com>
2019-08-17 04:07:20 +0100

    added sport to serializer

--- app/serializers/match_index_serializer.rb
+++ app/serializers/match_index_serializer.rb
@@ -1,4 +1,4 @@
 class MatchIndexSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :user_score, :opponent_name, :opponent_id, :opponent_score, :live, :user
+  attributes :user_score, :opponent_name, :opponent_id, :opponent_score, :live, :user, :sport
 end
